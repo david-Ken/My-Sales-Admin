54b84f701ed2c67e8370bf34f6a9bb6bac0cf0e9
David Keney <davidmyken@gmail.com>
2018-10-14 10:51:05 +0200

    third commit : added weather section with openweather API & custom icon depending on current weather description

---
+++ js/weather.js
@@ -0,0 +1,56 @@
+axios.get('http://api.openweathermap.org/data/2.5/weather?id=6440000&APPID=78885028b1b8985f9fa9f163c95d8232')
+    .then(function (response) {
+        // handle success
+        // alert(1);
+
+        var country = response.data.name;
+        var description = response.data.weather[0].description;
+        //var icon = response.data.weather[0].icon;
+        var temperature = parseInt(response.data.main.temp - 273) + '°';
+
+
+        $(".weather-country-name").html(country);
+        $(".weather-value").html(temperature);
+        $(".weather-description").html(description);
+
+        //Setting weather icon according to the current description
+        var iconElement = $(".weather-img");
+        switch (description) {
+            case 'clear sky':
+                iconElement.attr("src", "img/clear_sky.svg");
+                break;
+            case 'few clouds':
+                iconElement.attr("src", "img/few_cloud.svg");
+                break;
+            case 'scattered clouds':
+                iconElement.attr("src", "img/scattered_cloud.svg");
+                break;
+            case 'broken clouds':
+                iconElement.attr("src", "img/broken_cloud.svg");
+                break;
+            case 'shower rain':
+                iconElement.attr("src", "img/shower_rain.svg");
+                break;
+            case 'rain':
+                iconElement.attr("src", "img/rain.svg");
+                break;
+            case 'thunderstorm':
+                iconElement.attr("src", "img/thunderstorm.svg");
+                break;
+            case 'snow':
+                iconElement.attr("src", "img/snow.svg");
+                break;
+            case 'mist':
+                iconElement.attr("src", "img/mist.svg");
+                break;
+            default:
+                iconElement.attr("src", "img/climate.svg");
+        }
+    })
+    .catch(function (error) {
+        // handle error
+        alert(error);
+    })
+    .then(function () {
+        // always executed
+    });
