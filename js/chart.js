157fae5137db4dc746f0d894d89cd2429aa3b9da
david keney <davidmyken@gmail.com>
2018-11-28 20:50:28 +0100

    update : migrate from canvasJS to apexChart

--- js/chart.js
+++ js/chart.js
@@ -507,9 +507,63 @@ window.onload = function () {
 
     }
 
+
+
+
+
+
+
+
+    var options0 = {
+        chart: {
+            height: 350,
+            type: 'line',
+        },
+        series: [{
+            name: 'Clothes',
+            type: 'column',
+            data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
+        }, {
+            name: 'Electronics',
+            type: 'line',
+            data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
+        }],
+        stroke: {
+            width: [0, 4]
+        },
+        title: {
+            text: 'Products Sales'
+        },
+        //labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
+        xaxis: {
+            type: 'datetime'
+        },
+        yaxis: [{
+            title: {
+                text: 'Monthly',
+            },
+
+        }, {
+            opposite: true,
+            title: {
+                text: 'Weekly'
+            }
+        }]
+
+    }
+
+
+
+
     // Select charts div
 
-    var apexchart = new ApexCharts(
+    var apexchart0 = new ApexCharts(
+        document.querySelector("#apexChart0"),
+        options0
+    );
+
+    var apexchart1 = new ApexCharts(
         document.querySelector("#apexChart1"),
         options
     );
@@ -534,8 +588,9 @@ window.onload = function () {
     //render charts
 
     setTimeout(function () {
-        chart.render();
+        //chart.render();
         chart_icome.render();
+        apexchart0.render();
 
     }, 1000);
 
@@ -546,9 +601,10 @@ window.onload = function () {
     $(window).scroll(function () {
         $('.fade-chart-on-scroll').each(function () {
             if (isScrolledIntoView($(this)) && chartIsVisible === false) {
-                apexchart.render();
+                apexchart1.render();
                 apexchart2.render();
                 apexchart3.render();
+
                 chartIsVisible = true;
             }
         });
